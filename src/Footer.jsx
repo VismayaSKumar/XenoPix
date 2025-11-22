275d7926912eadd42a4ed5a50dd00a123c17ac0f
vismayaskumar <vismayaskumar10@gmail.com>
2025-11-22 13:20:56 +0530

    changed to the footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
   return (
     <div className='relative font-pixelify h-[60px] flex items-center justify-center'> 
       <div className='text-center'>made with 💌 by Vismaya</div>
-      <div className='absolute bottom-1 right-3 flex gap-3'>
-        <a href="https://www.linkedin.com/in/vismaya-s-33036b27b/"><img className='h-13' src={linkedin} alt="" /></a>
-        <a href="https://github.com/VismayaSKumar"><img className='h-13' src={github} alt="" /></a>
+      <div className='absolute bottom-1 right-3 flex gap-1 lg:gap-3'>
+        <a href="https://www.linkedin.com/in/vismaya-s-33036b27b/"><img className='h-7 lg:h-13' src={linkedin} alt="" /></a>
+        <a href="https://github.com/VismayaSKumar"><img className='h-7 lg:h-13' src={github} alt="" /></a>
       </div>
     </div>
   )
